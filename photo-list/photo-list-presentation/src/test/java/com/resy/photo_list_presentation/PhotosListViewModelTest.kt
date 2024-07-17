9a69f0beefc20ceeb26eb9d8537fadd778161d25
Mohamed-mekawy <mekawy2012@gmail.com>
2024-07-17 16:32:00 -0500

    fix failing test

--- photo-list/photo-list-presentation/src/test/java/com/resy/photo_list_presentation/PhotosListViewModelTest.kt
+++ photo-list/photo-list-presentation/src/test/java/com/resy/photo_list_presentation/PhotosListViewModelTest.kt
@@ -1,6 +1,5 @@
-package com.resy.photos.photosList.presentation
+package com.resy.photo_list_presentation
 
-import com.resy.domain.Results
 import com.resy.photo_list_domain.usecases.LoadPhotosListUseCase
 import io.mockk.coEvery
 import io.mockk.every
@@ -22,14 +21,14 @@ import org.junit.Test
 @OptIn(ExperimentalCoroutinesApi::class)
 class PhotosListViewModelTest {
 
-    private lateinit var loadPhotosUseCase: com.resy.photo_list_domain.usecases.LoadPhotosListUseCase
-    private lateinit var viewModel: com.resy.photo_list_presentation.PhotosListViewModel
+    private lateinit var loadPhotosUseCase: LoadPhotosListUseCase
+    private lateinit var viewModel: PhotosListViewModel
 
     @Before
     fun setup() {
         Dispatchers.setMain(StandardTestDispatcher())
         loadPhotosUseCase = mockk(relaxed = true)
-        viewModel = com.resy.photo_list_presentation.PhotosListViewModel(loadPhotosUseCase)
+        viewModel = PhotosListViewModel(loadPhotosUseCase)
     }
 
     @After
@@ -40,17 +39,17 @@ class PhotosListViewModelTest {
     @Test
     fun test_loading_state() = runTest {
         coEvery { loadPhotosUseCase() } returns flowOf(com.resy.domain.Results.Loading)
-        viewModel.handleAction(com.resy.photo_list_presentation.PhotosListUiAction.ReloadList)
-        assert(viewModel.profileUiState.value is com.resy.photo_list_presentation.PhotosUiState.Loading)
+        viewModel.handleAction(PhotosListUiAction.ReloadList)
+        assert(viewModel.profileUiState.value is PhotosUiState.Loading)
     }
 
     @Test
     fun test_error_state() = runTest {
         val throwable: Throwable = mockk(relaxed = true)
         coEvery { loadPhotosUseCase() } returns flowOf(com.resy.domain.Results.Error(throwable))
-        viewModel.handleAction(com.resy.photo_list_presentation.PhotosListUiAction.ReloadList)
+        viewModel.handleAction(PhotosListUiAction.ReloadList)
         advanceUntilIdle()
-        assert(viewModel.profileUiState.value is com.resy.photo_list_presentation.PhotosUiState.Error)
+        assert(viewModel.profileUiState.value is PhotosUiState.Error)
     }
 
     @Test
@@ -62,9 +61,9 @@ class PhotosListViewModelTest {
                 })
             )
         )
-        viewModel.handleAction(com.resy.photo_list_presentation.PhotosListUiAction.ReloadList)
+        viewModel.handleAction(PhotosListUiAction.ReloadList)
         advanceUntilIdle()
-        assert(viewModel.profileUiState.value is com.resy.photo_list_presentation.PhotosUiState.Success)
-        assertEquals((viewModel.profileUiState.value as com.resy.photo_list_presentation.PhotosUiState.Success).data.first().id, 32L)
+        assert(viewModel.profileUiState.value is PhotosUiState.Success)
+        assertEquals((viewModel.profileUiState.value as PhotosUiState.Success).data.first().id, 32L)
     }
 }
